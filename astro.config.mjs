0554d6c66e463f6e52a155b5da3bf6cd7163b23c
juanis2112 <juanitagomezr2112@gmail.com>
2025-07-28 16:33:08 -0700

    Fix config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juanitagomezr.github.io',
   base: '/juanitagomezr.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
